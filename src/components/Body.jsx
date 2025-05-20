5fdfd6842f9d142738956cead7b9c7347ba0dc08
SaadAlzarea <sfalzarea@gmail.com>
2025-05-20 09:02:18 +0300

    added border

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -7,21 +7,21 @@ function Body() {
         <p>14 Zones to Explore</p>
       </div>
       <section className="body">
-        <Card img="https://images.ctfassets.net/vy53kjqs34an/14DUIlTcHUbI5BXufzpwWy/36e1e391f3b2ea884fc58223c737c616/KV-Webook-Sizes_1280x1280.jpg?fm=webp&fit=crop&w=1280&h=1280" />
-        <Card img="https://images.ctfassets.net/vy53kjqs34an/5GbJPtJSfoTaw1qJmfgW7x/6b34d95bdeddf7be4a68dd9a52a1db84/1280_xs_1280_px.jpg?fm=webp&fit=crop&w=2667&h=2667" />
-        <Card img="https://images.ctfassets.net/vy53kjqs34an/6o10mUqReveeV2LNuoMaXb/fdad8ad178ac595c88df4d5d256d9aa4/Webook_SQUARE_THUMBNAIL_1280x1280.jpeg?fm=webp&fit=crop&w=1333&h=1333" />
-        <Card img="https://images.ctfassets.net/vy53kjqs34an/3g9mJ3niUxmQ2ILNG2gc1S/899ab054d863c9f333ac0f4da45c4204/1280_x_1280_-1.jpg?fm=webp&fit=crop&w=1280&h=1280" />
-        <Card img="https://images.ctfassets.net/vy53kjqs34an/37otv842YyHOCLvwlxglds/9bbf859ab4f6e720a5cca6c9afc1674f/WG_without_logo_WG_1280x1280.jpg?fm=webp&fit=crop&w=1280&h=1280" />
-        <Card img="https://images.ctfassets.net/vy53kjqs34an/75fr7DMO4OZVNDvmpv4cx5/5bc82c9a7880168f7cf98abf36ba6217/Square_Thumbnail.png?fm=webp&fit=crop&w=1281&h=1281" />
-        <Card img="https://images.ctfassets.net/vy53kjqs34an/5haOyVGvsqtKBVmYUnG4B/f6d7ce4bf4df3d67557ab73de29d5907/1280_xc_1280_px.jpg?fm=webp&fit=crop&w=2667&h=2667" />
-        <Card img="https://images.ctfassets.net/vy53kjqs34an/4xmbVpREbeZt8GyJY0pUBC/c8326ca5ef3cded900a02fcdf9183cde/1280_wwx_1280_px.jpg?fm=webp&fit=crop&w=2667&h=2667" />
-        <Card img="https://images.ctfassets.net/vy53kjqs34an/5wuBRcLYHSZSewUgypC9j8/65a09826b0bcbafa15584d1e9a0b5dbc/1280_x_128hh0_px.jpg?fm=webp&fit=crop&w=2667&h=2667" />
-        <Card img="https://images.ctfassets.net/vy53kjqs34an/3PFy5Mlcd0GaZO6WioJ9Rt/590b72cbf5bf63fcbd9eb3176e02a92c/1280_wx_1280_px.jpg?fm=webp&fit=crop&w=2667&h=2667" />
-        <Card img="https://images.ctfassets.net/vy53kjqs34an/3PFy5Mlcd0GaZO6WioJ9Rt/590b72cbf5bf63fcbd9eb3176e02a92c/1280_wx_1280_px.jpg?fm=webp&fit=crop&w=2667&h=2667" />
-        <Card img="https://images.ctfassets.net/vy53kjqs34an/2oAxloGPIpFLjbS7zfJniB/1291d75013dface3bc0e1242c097541f/WhatsApp_Image_2024-11-25_at_18.08.58_acb72643.jpg?fm=webp&fit=crop&w=1280&h=1280" />
-        <Card img="https://images.ctfassets.net/vy53kjqs34an/56GwAx2tlRijk85k0ZPvtU/72a1405def7784caa597d641e8779972/1280x1280.jpg?fm=webp&fit=crop&w=1280&h=1280" />
-        <Card img="https://images.ctfassets.net/vy53kjqs34an/6TcaMYEPFZpJKNfmx1ylLU/7ac2f8fea5723e6856bcba87e4198ef6/1280X1280.jpg?fm=webp&fit=crop&w=1280&h=1280" />
-        <Card img="https://images.ctfassets.net/vy53kjqs34an/2qOMbX4oLfqrt0JZcurgVm/1dbf0eb82e82116c1ac3f2083bcd1432/1280_x_1280_px.jpg?fm=webp&fit=crop&w=2667&h=2667" />
+        <Card new={false} img="https://images.ctfassets.net/vy53kjqs34an/14DUIlTcHUbI5BXufzpwWy/36e1e391f3b2ea884fc58223c737c616/KV-Webook-Sizes_1280x1280.jpg?fm=webp&fit=crop&w=1280&h=1280"  />
+        <Card new={false} img="https://images.ctfassets.net/vy53kjqs34an/5GbJPtJSfoTaw1qJmfgW7x/6b34d95bdeddf7be4a68dd9a52a1db84/1280_xs_1280_px.jpg?fm=webp&fit=crop&w=2667&h=2667" />
+        <Card new={false} img="https://images.ctfassets.net/vy53kjqs34an/6o10mUqReveeV2LNuoMaXb/fdad8ad178ac595c88df4d5d256d9aa4/Webook_SQUARE_THUMBNAIL_1280x1280.jpeg?fm=webp&fit=crop&w=1333&h=1333" />
+        <Card new={true} img="https://images.ctfassets.net/vy53kjqs34an/3g9mJ3niUxmQ2ILNG2gc1S/899ab054d863c9f333ac0f4da45c4204/1280_x_1280_-1.jpg?fm=webp&fit=crop&w=1280&h=1280" />
+        <Card new={false} img="https://images.ctfassets.net/vy53kjqs34an/37otv842YyHOCLvwlxglds/9bbf859ab4f6e720a5cca6c9afc1674f/WG_without_logo_WG_1280x1280.jpg?fm=webp&fit=crop&w=1280&h=1280" />
+        <Card new={false} img="https://images.ctfassets.net/vy53kjqs34an/75fr7DMO4OZVNDvmpv4cx5/5bc82c9a7880168f7cf98abf36ba6217/Square_Thumbnail.png?fm=webp&fit=crop&w=1281&h=1281" />
+        <Card new={false} img="https://images.ctfassets.net/vy53kjqs34an/5haOyVGvsqtKBVmYUnG4B/f6d7ce4bf4df3d67557ab73de29d5907/1280_xc_1280_px.jpg?fm=webp&fit=crop&w=2667&h=2667" />
+        <Card new={false} img="https://images.ctfassets.net/vy53kjqs34an/4xmbVpREbeZt8GyJY0pUBC/c8326ca5ef3cded900a02fcdf9183cde/1280_wwx_1280_px.jpg?fm=webp&fit=crop&w=2667&h=2667" />
+        <Card new={true} img="https://images.ctfassets.net/vy53kjqs34an/5wuBRcLYHSZSewUgypC9j8/65a09826b0bcbafa15584d1e9a0b5dbc/1280_x_128hh0_px.jpg?fm=webp&fit=crop&w=2667&h=2667" />
+        <Card new={false} img="https://images.ctfassets.net/vy53kjqs34an/3PFy5Mlcd0GaZO6WioJ9Rt/590b72cbf5bf63fcbd9eb3176e02a92c/1280_wx_1280_px.jpg?fm=webp&fit=crop&w=2667&h=2667" />
+        <Card new={false} img="https://images.ctfassets.net/vy53kjqs34an/3PFy5Mlcd0GaZO6WioJ9Rt/590b72cbf5bf63fcbd9eb3176e02a92c/1280_wx_1280_px.jpg?fm=webp&fit=crop&w=2667&h=2667" />
+        <Card new={false} img="https://images.ctfassets.net/vy53kjqs34an/2oAxloGPIpFLjbS7zfJniB/1291d75013dface3bc0e1242c097541f/WhatsApp_Image_2024-11-25_at_18.08.58_acb72643.jpg?fm=webp&fit=crop&w=1280&h=1280" />
+        <Card new={false} img="https://images.ctfassets.net/vy53kjqs34an/56GwAx2tlRijk85k0ZPvtU/72a1405def7784caa597d641e8779972/1280x1280.jpg?fm=webp&fit=crop&w=1280&h=1280" />
+        <Card new={false} img="https://images.ctfassets.net/vy53kjqs34an/6TcaMYEPFZpJKNfmx1ylLU/7ac2f8fea5723e6856bcba87e4198ef6/1280X1280.jpg?fm=webp&fit=crop&w=1280&h=1280" />
+        <Card new={false} img="https://images.ctfassets.net/vy53kjqs34an/2qOMbX4oLfqrt0JZcurgVm/1dbf0eb82e82116c1ac3f2083bcd1432/1280_x_1280_px.jpg?fm=webp&fit=crop&w=2667&h=2667" />
       </section>
       <div className="sec-3">
         <div className="left-side">
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import "./Card.css";
 function card(props) {
   return (
     <section className="sec-image">
-      <div className="card">
+      <div className="card" style={{border: props.new ? "2px green solid":""}}>
         <img className="" src={props.img} alt="" />
       </div>
     </section>
